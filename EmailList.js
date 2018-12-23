3f23389d7e62ed0637b869ce107217f4820acb21
Timingtheworld <timm12334@gmail.com>
2018-12-23 19:52:38 +0800

    Delete EmailList.js

--- EmailList.js
+++
@@ -1,4 +0,0 @@
-exports.EmailList = function (){
-    var list=['timlai@tokenite.co','steven7630@gmail.com','chun060608@gmail.com','wayne265265@gmail.com','tsengyuhan@gmail.com','vanessa.yao1111@gmail.com','enjinlin@gmail.com']
-    return list
-}
