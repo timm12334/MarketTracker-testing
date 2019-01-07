0e2d4c6467cf95e66fdb8b528a38cd1677d869b5
Timingtheworld <timm12334@gmail.com>
2019-01-07 16:53:10 +0800

    Add files via upload

---
+++ insertdata.js
@@ -0,0 +1,14 @@
+exports.insertdata=function(collectionname,datatoinsert){
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("admin");
+  dbo.collection(collectionname).insertMany(datatoinsert, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
+} 
